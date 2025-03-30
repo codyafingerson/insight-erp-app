46fa34c798190acb0dd7e03bace6ac70b2075224
Cody Fingerson <codyfingerson0428@gmail.com>
2025-03-30 00:14:02 -0600

    refactor(Navbar): remove unused imports and streamline component

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,13 +1,10 @@
 import { useAppDispatch } from "../../app/hooks"
 import { useAuth } from "../../features/auth/useAuth"
 import { logout } from "../../features/auth/authSlice"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
 
 export default function Navbar() {
     const dispatch = useAppDispatch()
     const { user, isAuthenticated } = useAuth()
-    const navigate = useNavigate()
 
     if (!isAuthenticated) {
         return null;
